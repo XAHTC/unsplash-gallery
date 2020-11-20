5d2e07c799310c685ce16da1a68886f795c8b960
XAHTC <34131934+XAHTC@users.noreply.github.com>
2020-11-20 22:15:41 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Modal from "./components/Modal";
+
+import "./style.css";
 
 function App() {
+  const [item, setItem] = useState([]);
+
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  useEffect(() => {
+    fetch(
+      "https://api.unsplash.com/photos/?client_id=cf49c08b444ff4cb9e4d126b7e9f7513ba1ee58de7906e4360afc1a33d1bf4c0"
+    )
+      .then((res) => res.json())
+      .then((result) => setItem(result));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      {item.map((item, index) => (
+        <div
+          className="image-container"
+          key={item.id}
+          id={index}
+          onClick={() => setSelectedItem(item)}
         >
-          Learn React
-        </a>
-      </header>
+          <img className="image" src={item.urls.regular} alt="logo" />
+          <div className="info">
+            <img
+              className="avatar"
+              src={item.user.profile_image.small}
+              alt="avatar"
+            />
+            <div className="text">
+              <p className="name">{item.user.username}</p>
+              <p className="name">{item.alt_description}</p>
+            </div>
+          </div>
+        </div>
+      ))}
+      {selectedItem && (
+        <Modal
+          active={true}
+          onClose={() => setSelectedItem(null)}
+          url={selectedItem.urls.regular}
+        />
+      )}
     </div>
   );
 }
-
 export default App;

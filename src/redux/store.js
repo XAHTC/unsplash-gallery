e6697b056f4884e7c67876c55297f1ad2df23bf0
XAHTC <34131934+XAHTC@users.noreply.github.com>
2020-11-21 13:56:14 +0200

    deleted redux webtools

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,7 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore } from "redux";
 import rootReducer from "./reducers";
 import thunk from "redux-thunk";
 
-const store = createStore(
-  rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+const store = createStore(rootReducer, thunk);
 
 export default store;
